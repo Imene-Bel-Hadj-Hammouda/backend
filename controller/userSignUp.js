7b37ada317c54e59b23e90bfb5f71cac406c2616
Bel Hadj Hammouda Imene <bel.hadj.hammouda.imene@gmail.com>
2024-10-21 01:09:30 -1200

    add models and controller

---
+++ controller/userSignUp.js
@@ -0,0 +1,12 @@
+
+async function userSignUpController(req,res) {
+    try {
+
+    }catch (error){
+        res.json({
+            message: error,
+            err: true,
+            success: false,
+        })
+    }
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const connectDB = require('../backend/config/db')
+//const router= require('./routes')
+const router = express.Router();
+
+const app = express()
+app.use(cors())
+app.use("/api",router)
+
+const PORT = 8080 || process.env.PORT
+connectDB().then(()=> {
+    app.listen(PORT,()=>{
+        console.log("**** Server is running ****")
+        console.log('*^_^* connect to DB *^_^*')
+    })
+}
+)
+
